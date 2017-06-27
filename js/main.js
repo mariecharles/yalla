a8e52694a8b10cdab4c588406ccbf04cb44feb26
alisonsayarath <alison.sayarath@hetic.net>
2017-06-27 10:24:49 +0200

    page action done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
+var first_section = document.querySelectorAll('section')[0];
 var header = document.querySelector('header');
 var body = document.querySelector('body');
 var current_position = 0;
 var last_position = 0;
 
-var lol = document.querySelector('#intro');
-console.log(lol.offsetHeight);
+// var lol = document.querySelector('#intro');
+
 
 window.addEventListener('scroll', function(e) {
     current_position = body.scrollTop;
 
-    if (current_position < (lol.offsetHeight - 200) || current_position == 0) {
-      header.className = '';
-    } else if (current_position < last_position || current_position == last_position) {
+    if (body.id != 'index') {
       header.className = 'not-top';
-    } else if (current_position > lol.offsetHeight){
-      header.className = 'active';
     }
+
+    // if (current_position < (first_section.offsetHeight - 200) || current_position == 0) {
+    //   header.className = '';
+    // } else if (current_position < last_position || current_position == last_position) {
+    //   header.className = 'not-top';
+    // } else if (current_position > first_section.offsetHeight){
+    //   header.className = 'active';
+    // }
 
     last_position = body.scrollTop;
 });
